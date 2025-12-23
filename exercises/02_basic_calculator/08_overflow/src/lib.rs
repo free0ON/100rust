fac5f834567b4f734ffa220032990ec7abfd4287
free0ON <8333184@gmail.com>
2025-12-23 11:08:18 +0300

    02_basic_calculator done

--- exercises/02_basic_calculator/08_overflow/src/lib.rs
+++ exercises/02_basic_calculator/08_overflow/src/lib.rs
@@ -9,9 +9,9 @@ pub fn factorial(n: u32) -> u32 {
     if n == 0 {
         return 1;
     }
-    let mut result = 1;
+    let mut result: u32 = 1;
     for i in 1..=n {
-        result *= i;
+        result = result.wrapping_mul(i);
     }
     result
 }
